c72f6cc51f437c8d37da51ee85d2b0596970b4fc
gn.fur <patych7@gmail.com>
2018-03-21 21:26:13 +0300

    Finished base font functionality.

--- Monofoxe/Monofoxe/Engine/Drawing/Fonts.cs
+++ Monofoxe/Monofoxe/Engine/Drawing/Fonts.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using System.Diagnostics;
-using Monofoxe.Engine.Drawing;
 
 namespace Monofoxe.Engine.Drawing
 {
 	public static class Fonts
 	{
-		//SpriteFont
-		public static IFont TexFont = new TextureFont(Sprites.SpriteFont, " !" + '"' + @"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~", true);
+		static string ascii = " !" + '"' + @"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
+		
+		public static IFont TexFont = new TextureFont(Sprites.SpriteFont, 3, 3, ascii, false);
+		public static IFont AnotherFont = new TextureFont(Sprites.AnotherFont, 1, 1, ascii, false);
+
+		public static IFont AnotherFont1 = new TextureFont(Sprites.AnotherFont, 1, 1, ascii, true);
 	}
 }
