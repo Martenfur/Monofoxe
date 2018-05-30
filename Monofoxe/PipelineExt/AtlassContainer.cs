3fde92b3d5696278ce9330278371e0a013a57c0a
gn Fur <21219159+gnFur@users.noreply.github.com>
2018-05-30 15:43:22 +0300

    Removed some redundant classes from PipelineExt, added exceptions and comments.

---
+++ Monofoxe/PipelineExt/AtlassContainer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework.Content.Pipeline.Graphics;
+
+namespace PipelineExt
+{
+	/// <summary>
+	/// Basic atlass container. Stores list of frames or sprites and their texture. 
+	/// </summary>
+	/// <typeparam name="T">Frame or Sprite.</typeparam>
+	public class AtlassContainer<T>
+	{
+		public List<T> Items;
+		public TextureContent Texture;
+
+		public AtlassContainer() =>
+			Items = new List<T>();
+		
+		public void Add(T item) =>
+			Items.Add(item);
+	}
+}
