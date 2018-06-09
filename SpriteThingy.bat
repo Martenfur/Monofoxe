835dc5de148b397a65166b2ac0582d2dae857f90
gn Fur <21219159+gnFur@users.noreply.github.com>
2018-06-09 23:42:24 +0300

    Reformatted project and added sprite grouping support.

--- SpriteThingy.bat
+++ SpriteThingy.bat
@@ -1,1 +1,1 @@
-SpriteThingy \TestGraphics\ \Monofoxe\Monofoxe\Content\Atlasses\ \TestGraphics\Sprites_template.cs \Monofoxe\Monofoxe\Engine\Drawing\Sprites.cs "TexturePacker --format json --disable-rotation --multipack --trim-sprite-names --algorithm MaxRects --data <output_dir>texture_{n}.json <input_dir>"
+SpriteThingy \TestGraphics\ \Monofoxe\Monofoxe\Content\Atlasses\ \TestGraphics\Sprites_template.cs \Monofoxe\Monofoxe\Sprites\ "TexturePacker --format json --disable-rotation --multipack --trim-sprite-names --algorithm MaxRects --data <output_dir><atlass_name>_{n}.json <input_dir>"
--- TestGraphics/Sprites_template.cs
+++ TestGraphics/Sprites_template.cs
@@ -1,25 +1,64 @@
 #class_prefix = "public static Sprite <sprite_name>;"
-#class_constructor = "<sprite_name> = new Sprite(frames[<hash_sprite_name>], <args>);"
+#class_constructor = "<sprite_name> = new Sprite(sprites[<hash_sprite_name>], <args>);"
+//Template: <group_name> is a built-in variable.
 
+using Microsoft.Xna.Framework.Content;
+using Monofoxe.Engine;
+using Monofoxe.Engine.Drawing;
 using System;
 using System.Collections.Generic;
-using Monofoxe.Engine.Drawing;
+using System.Linq;
 
-namespace Monofoxe.Engine.Drawing
+namespace Sprites
 {
-	public static class Sprites
+	public static class <group_name>
 	{
 		#region sprites
 		<class_prefix>		
 		#endregion sprites
 		
-		public static void Init(Dictionary<string, Frame[]> frames)
-		{	
+		private static string _groupName = "<group_name>";
+		private static ContentManager _content = new ContentManager(GameCntrl.Game.Services);
+		
+		public static bool Loaded = false;
+		
+		public static void Load()
+		{
+			var sprites = new Dictionary<string, Frame[]>();
+			
+			var i = 0;
+			var graphicsPath = GameCntrl.ContentDir + '/' + GameCntrl.GraphicsDir +  '/' + _groupName + '_';
+			
+			Loaded = true;
+			
+			// Loading all atlasses.
+			while(true)
+			{
+				try
+				{
+					var atlassSprites = _content.Load<Dictionary<string, Frame[]>>(graphicsPath + i);
+					sprites = sprites.Concat(atlassSprites).ToDictionary(x => x.Key, x => x.Value);
+				}
+				catch(Exception) // If content file doesn't exist, this means we've loaded all atlasses.
+				{
+					break;
+				}
+
+				i += 1;
+			}
+			// Loading all atlasses.
+			
 			#region sprite_constructors
 
 			<class_constructor>
 
 			#endregion sprite_constructors
+		}
+		
+		public static void Unload()
+		{
+			_content.Unload();
+			Loaded = false;
 		}
 	}
 }
