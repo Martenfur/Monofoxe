af3694d8373af5c32d79568698847a3a29e30cf1
gn.fur <patych7@gmail.com>
2018-07-21 01:04:50 +0300

    Sound... stuff.

--- Monofoxe/MonofoxeCore/Engine/Audio/AudioMgr.cs
+++ Monofoxe/MonofoxeCore/Engine/Audio/AudioMgr.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.IO;
 using System.Runtime.InteropServices;
+using Microsoft.Xna.Framework;
 
 namespace Monofoxe.Engine.Audio
 {
-	
+	// TODO: FIgure out how to make this stuff cross-platform.
+	// Maybe add an interface and make a singleton?
+
 	public static class AudioMgr
 	{
 		private static FMOD.System _FMODSystem;
@@ -31,11 +34,24 @@ namespace Monofoxe.Engine.Audio
 
 			FMOD.Factory.System_Create(out _FMODSystem);
 			_FMODSystem.setDSPBufferSize(1024, 10);
-			_FMODSystem.init(32, FMOD.INITFLAGS.CHANNEL_LOWPASS, (IntPtr)0);
+			_FMODSystem.init(32, FMOD.INITFLAGS.CHANNEL_LOWPASS | FMOD.INITFLAGS.CHANNEL_DISTANCEFILTER, (IntPtr)0);
 			
+			var pos = GetFmodVector(32, 32, 0);
+			var vel = GetFmodVector(0, 0, 0);
+			var forw = GetFmodVector(1, 0, 0);
+			var up = GetFmodVector(0, 0, 1);
+			_FMODSystem.set3DNumListeners(1);
+			_FMODSystem.set3DListenerAttributes(1, ref pos, ref vel, ref forw, ref up);
 			
 		}
 		
+		public static FMOD.ChannelGroup CreateChannelGroup(string name)
+		{
+			FMOD.ChannelGroup group;
+			_FMODSystem.createChannelGroup(name, out group);
+			return group;
+		}
+
 
 		public static void Unload() =>
 			_FMODSystem.release();
@@ -45,6 +61,7 @@ namespace Monofoxe.Engine.Audio
 			_FMODSystem.update();
 		
 		
+
 		public static Sound LoadSound(string name, FMOD.MODE mode = FMOD.MODE.DEFAULT)
 		{
 			FMOD.Sound newSound;
@@ -61,7 +78,24 @@ namespace Monofoxe.Engine.Audio
 			return new Sound(_FMODSystem, newSound);
 		}
 
-		
+		public static FMOD.VECTOR GetFmodVector(float x, float y, float z)
+		{
+			var vector = new FMOD.VECTOR();
+			vector.x = x;
+			vector.y = y;
+			vector.z = z;
+			return vector;
+		}
+
+		public static FMOD.VECTOR GetFmodVector(Vector3 v)
+		{
+			var vector = new FMOD.VECTOR();
+			vector.x = v.X;
+			vector.y = v.Y;
+			vector.z = v.Z;
+			return vector;
+		}
+
 	//	public void Play(bool paused = false) =>
 		//	LastResult = _FMODSystem.playSound(_sound, null, paused, out _channel);
 
