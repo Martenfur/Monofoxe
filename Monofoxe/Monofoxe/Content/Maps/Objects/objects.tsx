55e916a22467eff81325cb3c3526b7d8681e096f
gn Fur <21219159+gnFur@users.noreply.github.com>
2018-10-27 00:17:44 +0300

    Pretty much finished tileset parser.

--- Monofoxe/Monofoxe/Content/Maps/Objects/objects.tsx
+++ Monofoxe/Monofoxe/Content/Maps/Objects/objects.tsx
@@ -1,7 +1,10 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.0" name="objects" tilewidth="54" tileheight="57" tilecount="1" columns="0">
+<tileset version="1.2" tiledversion="1.2.0" name="objects" tilewidth="126" tileheight="57" tilecount="2" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="54" height="57" source="barrel.png"/>
+ </tile>
+ <tile id="1">
+  <image width="126" height="32" source="bench.png"/>
  </tile>
 </tileset>
---
+++ Monofoxe/Pipefoxe/Tiled/TiledMapImporter.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using Monofoxe.Tiled.MapStructure;
+using Microsoft.Xna.Framework.Content.Pipeline;
+using System.Xml;
+using System.Text;
+using System.IO;
+using Microsoft.Xna.Framework;
+
+namespace Pipefoxe.Tiled
+{
+	[ContentImporter(".tmx", DefaultProcessor = "PassThroughProcessor", 
+	DisplayName = "Tiled Map Importer - Monofoxe")]
+	public class TiledMapImporter : ContentImporter<TiledMap>
+	{
+		static StringBuilder _logs = new StringBuilder();
+
+		public static string RootDir;
+
+		public override TiledMap Import(string filename, ContentImporterContext context)
+		{
+			RootDir = Path.GetDirectoryName(filename) + '/';
+			
+			var xml = new XmlDocument();
+			xml.Load(filename);
+
+			try
+			{
+				var map = MapParser.Parse(xml);
+
+				__SaveLog(RootDir);
+				return map;
+			}
+			catch(Exception e)
+			{
+				__SaveLog(RootDir);
+				throw e;
+			}
+		}
+
+
+
+		
+		public static void __Log(string text)
+		{
+			_logs.Append(text + Environment.NewLine);
+		}
+
+
+
+		public static void __SaveLog(string path)
+		{
+			File.WriteAllText(path + "log.log", _logs.ToString());
+		}
+
+
+
+		/// <summary>
+		/// Converts string in hex format #RRGGBB or #AARRGGBB to Color.
+		/// </summary>
+		public static Color StringToColor(string colorStr)
+		{
+			colorStr = colorStr.Replace("#", "");
+
+			var channels = new byte[colorStr.Length / 2];
+
+			for(var i = 0; i < channels.Length; i += 1)
+			{
+				channels[i] = Convert.ToByte(colorStr.Substring(i * 2, 2), 16);
+			}
+
+			if (channels.Length == 3)
+			{
+				// #RRGGBB
+				return new Color(channels[0], channels[1], channels[2]);
+			}
+			else
+			{
+				// #AARRGGBB
+				return new Color(channels[1], channels[2], channels[3], channels[0]);
+			}
+		}
+
+		/// <summary>
+		/// Returns properties from xml node. 
+		/// </summary>
+		public static Dictionary<string, string> GetProperties(XmlNode node)
+		{
+			var dictionary = new Dictionary<string, string>();
+			
+			if (node["properties"] != null)
+			{
+				try
+				{
+					var nodeList = node["properties"].SelectNodes("property");
+					foreach(XmlNode propertyXml in nodeList)
+					{
+						dictionary.Add(propertyXml.Attributes["name"].Value, propertyXml.Attributes["value"].Value);
+					}
+				}
+				catch(Exception e)
+				{
+					throw new Exception("Error while parsing properties!" + e.StackTrace);
+				}
+			}
+			
+			return dictionary;
+		}
+
+	}
+}
