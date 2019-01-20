fe53fec6ebc8b7c4590836d7784dd422cc3e57ea
gn.fur <patych7@gmail.com>
2019-01-20 11:54:37 +0300

    Updated hgignore, finished with the release script.

--- PackRelease.ps1
+++ PackRelease.ps1
@@ -1,6 +1,6 @@
-Add-Type -A System.IO.Compression.FileSystem
-Add-Type -A System
+# NOTE: Script doesn't build projects. You need to build them into release yourself before running it.
 
+Add-Type -A System.IO.Compression.FileSystem
 
 $srcLibDir = "$PWD\Monofoxe\bin\Release"
 $srcPipelineLibDir = "$PWD\Monofoxe\bin\Pipeline\Release"
@@ -13,35 +13,40 @@ $desktopGL = "MonofoxeDesktopGL"
 $desktopGLTemplate = "$PWD\Templates\$desktopGL\"
 
 
-# Cleaning old dir.
-if (Test-Path $destReleaseDir -PathType Container)
+"Cleaning output directory at $destReleaseDir..."
+if (Test-Path "$destReleaseDir" -PathType Container)
 {
-	Remove-Item $destReleaseDir -Force -Recurse
+	Remove-Item "$destReleaseDir" -Force -Recurse
 }
-New-Item -ItemType Directory -Force -Path $destReleaseDir
-# Cleaning old dir.
+New-Item -ItemType Directory -Force -Path "$destReleaseDir" > $null
 
 
-# Copying DesktopGL template.
-Copy-Item -path $desktopGLTemplate -Destination $destReleaseDir -Recurse -Container
-# Copying libraries inside.
-Copy-Item -path $srcLibDir\* -Filter "*.dll" -Destination "$destReleaseDir$desktopGL\References\"
-Copy-Item -path $srcPipelineLibDir\* -Filter "*.dll" -Destination "$destReleaseDir$desktopGL\Content\References\"
-Copy-Item -path $srcFMODLibDir -Destination "$destReleaseDir$desktopGL\" -Recurse
+"Copying templates from $desktopGLTemplate..."
+Copy-Item -path "$desktopGLTemplate" -Destination "$destReleaseDir" -Recurse -Container
+
+"Copying libraries for templates from $desktopGLTemplate..."
+New-Item -ItemType Directory -Force -Path "$destReleaseDir$desktopGL\References\" > $null
+Copy-Item -path "$srcLibDir\*" -Filter "*.dll" -Destination "$destReleaseDir$desktopGL\References\"
+New-Item -ItemType Directory -Force -Path "$destReleaseDir$desktopGL\Content\References\" > $null
+Copy-Item -path "$srcPipelineLibDir\*" -Filter "*.dll" -Destination "$destReleaseDir$desktopGL\Content\References\"
+Copy-Item -path "$srcFMODLibDir" -Destination "$destReleaseDir$desktopGL\" -Recurse
 
 
+"Copying raw libraries..."
+New-Item -ItemType Directory -Force -Path "$destLibDir" > $null
+Copy-Item -path "$srcLibDir\*" -Filter "*.dll" -Destination "$destLibDir"
+New-Item -ItemType Directory -Force -Path "$destLibDir\Pipeline\" > $null
+Copy-Item -path "$srcPipelineLibDir\*" -Filter "*.dll" -Destination "$destLibDir\Pipeline\"
+Copy-Item -path "$srcFMODLibDir" -Destination $destLibDir -Recurse
 
-New-Item -ItemType Directory -Force -Path $destLibDir
-Copy-Item -path $srcLibDir\* -Filter "*.dll" -Destination $destLibDir
-New-Item -ItemType Directory -Force -Path "$destLibDir\Pipeline\"
-Copy-Item -path $srcPipelineLibDir\* -Filter "*.dll" -Destination "$destLibDir\Pipeline\"
-Copy-Item -path $srcFMODLibDir -Destination $destLibDir -Recurse
 
-
+"Packing templates..."
 [IO.Compression.ZipFile]::CreateFromDirectory("$destReleaseDir$desktopGL", "$destReleaseDir$desktopGL.zip")
+"Packing raw libraries..."
 [IO.Compression.ZipFile]::CreateFromDirectory("$destLibDir", "$destLibDir.zip")
 
+"Cleaning..."
 Remove-Item "$destReleaseDir$desktopGL" -Force -Recurse
 Remove-Item "$destLibDir" -Force -Recurse
 
-Read-Host -Prompt "Done!"
+Read-Host -Prompt "Done! Press Enter to exit"
