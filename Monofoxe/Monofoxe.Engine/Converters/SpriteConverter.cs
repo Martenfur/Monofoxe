f2a353d66b5ff1332cd4501eefdf91b785f60582
gn Fur <21219159+gnFur@users.noreply.github.com>
2019-02-01 21:18:53 +0300

    Modified sprite JSON converter.

--- Monofoxe/Monofoxe.Engine/Converters/SpriteConverter.cs
+++ Monofoxe/Monofoxe.Engine/Converters/SpriteConverter.cs
@@ -14,6 +14,11 @@ namespace Monofoxe.Engine.Converters
 	/// </summary>
 	public class SpriteConverter : BasicConverter
 	{
+		/// <summary>
+		/// Suggested namespaces for sprite class. 
+		/// </summary>
+		internal string[] _namespaces = {""};
+
 		public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer) =>
 			throw new InvalidOperationException("Cannot serialize sprites!");
 
@@ -25,8 +30,31 @@ namespace Monofoxe.Engine.Converters
 			var fieldName = words[words.Length - 1];
 			var className = name.Substring(0, name.Length - fieldName.Length - 1);
 
-			var type = Type.GetType(className + ", " + Assembly.GetEntryAssembly(), true);
-			
+
+			Type type = null;
+			try
+			{
+				type = Type.GetType(className + ", " + Assembly.GetEntryAssembly(), true);
+			}
+			catch(TypeLoadException)
+			{
+				// If type isn't found, check all suggested namespaces.
+				foreach(var spriteNamespace in _namespaces)
+				{
+					try
+					{
+						type = Type.GetType(spriteNamespace + "." + className + ", " + Assembly.GetEntryAssembly(), true);
+						break; // Break, if type was successfully found. Won't be executed on exception.
+					}
+					catch(TypeLoadException){}
+				}
+				if (type == null)
+				{
+					throw new TypeLoadException("Failed to load sprite from " + className + " - no namespaces match.");
+				}
+			}
+
+
 			return type.GetField(fieldName).GetValue(null);
 		}
 	}
