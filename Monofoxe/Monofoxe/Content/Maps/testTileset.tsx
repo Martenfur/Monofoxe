acbbcff319b898c59a5392e1ef617d5dff378367
gn.fur <patych7@gmail.com>
2019-02-08 13:55:20 +0300

    Tested tileset offsets.

--- Monofoxe/Monofoxe/Content/Maps/testTileset.tsx
+++ Monofoxe/Monofoxe/Content/Maps/testTileset.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.1" name="testTileset" tilewidth="32" tileheight="32" tilecount="16" columns="4">
+<tileset version="1.2" tiledversion="1.2.2" name="testTileset" tilewidth="32" tileheight="32" tilecount="16" columns="4">
  <image source="testTileset.png" width="128" height="128"/>
  <terraintypes>
   <terrain name="New Terrain" tile="5"/>
