84fa6a801aa2364b7768af9260b07a4066455195
msmshazan <msmshazan@localhost>
2019-06-19 22:40:40 +0530

    Added Commonfiles to template folder and proper fix for the problem

---
+++ Templates/CommonFiles/Resources/Effects.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Resources.Sprites;
+using Monofoxe.Engine.Drawing;
+using Monofoxe.Engine;
+
+namespace Resources
+{
+	public static class Effects
+	{
+		private static ContentManager _content;
+		
+
+		public static void Load()
+		{
+			_content = new ContentManager(GameMgr.Game.Services);
+			_content.RootDirectory = AssetMgr.ContentDir + '/' + AssetMgr.EffectsDir;
+			
+		}
+
+		public static void Unload()
+		{
+			_content.Unload();
+		}
+
+	}
+}
