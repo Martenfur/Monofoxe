b673fee98352c81aec98210bc62e6f3216f2ff8f
gn.fur <patych7@gmail.com>
2019-06-20 01:45:21 +0300

    Added Monofoxe.Playground solution and some basic code.

---
+++ Monofoxe.Playground/Monofoxe.Playground/GraphicsDemo/ShapeDemo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Monofoxe.Engine.ECS;
+using Monofoxe.Engine.Drawing;
+using Monofoxe.Engine.SceneSystem;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+namespace Monofoxe.Playground.GraphicsDemo
+{
+	public class ShapeDemo : Entity
+	{
+		public ShapeDemo(Layer layer) : base(layer)
+		{
+			
+		}
+
+		public override void Draw()
+		{	
+			CircleShape.Draw(new Vector2(100, 100), 100, true);
+		}
+
+	}
+}
---
+++ Monofoxe.Playground/Monofoxe.Playground/Interface/SceneSwitcher.cs
@@ -0,0 +1,54 @@
+﻿using Monofoxe.Engine.ECS;
+using Monofoxe.Engine.SceneSystem;
+using System.Collections.Generic;
+using Monofoxe.Playground.GraphicsDemo;
+
+namespace Monofoxe.Playground.Interface
+{
+	public static class SceneSwitcher
+	{
+		public static List<SceneFactory> Factories = new List<SceneFactory>
+		{
+			new GraphicsDemoFactory(),
+
+		};
+
+		public static int CurrentSceneID {get; private set;} = 0;
+
+		public static Scene CurrentScene => Factories[CurrentSceneID].Scene;
+
+		public static SceneFactory CurrentFactory => Factories[CurrentSceneID];
+
+		public static void NextScene()
+		{
+			Factories[CurrentSceneID].DestroyScene();
+
+			CurrentSceneID += 1;
+			if (CurrentSceneID >= Factories.Count)
+			{
+				CurrentSceneID = 0;
+			}
+
+			Factories[CurrentSceneID].CreateScene();
+		}
+
+
+		public static void PreviousScene()
+		{
+			Factories[CurrentSceneID].DestroyScene();
+
+			CurrentSceneID -= 1;
+			if (CurrentSceneID < 0)
+			{
+				CurrentSceneID = Factories.Count - 1;
+			}
+
+			Factories[CurrentSceneID].CreateScene();
+		}
+
+
+		public static void RestartScene() =>
+			Factories[CurrentSceneID].RestartScene();
+
+	}
+}
