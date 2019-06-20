4c1371717d9c1ac995aa2a30939b55c63375d7b6
gn Fur <21219159+gnFur@users.noreply.github.com>
2019-06-20 18:36:08 +0300

    Fixed text being drawn last in the frame crashing the game.

--- Monofoxe.Playground/Monofoxe.Playground/Interface/SceneSwitcher.cs
+++ Monofoxe.Playground/Monofoxe.Playground/Interface/SceneSwitcher.cs
@@ -2,24 +2,54 @@
 using Monofoxe.Engine.SceneSystem;
 using System.Collections.Generic;
 using Monofoxe.Playground.GraphicsDemo;
+using Microsoft.Xna.Framework;
+using Monofoxe.Engine.Drawing;
+using Monofoxe.Engine;
+
 
 namespace Monofoxe.Playground.Interface
 {
-	public static class SceneSwitcher
+	public class SceneSwitcher : Entity
 	{
-		public static List<SceneFactory> Factories = new List<SceneFactory>
+		public List<SceneFactory> Factories = new List<SceneFactory>
 		{
 			new GraphicsDemoFactory(),
 
 		};
 
-		public static int CurrentSceneID {get; private set;} = 0;
+		public int CurrentSceneID {get; private set;} = 0;
+		public Scene CurrentScene => Factories[CurrentSceneID].Scene;
+		public SceneFactory CurrentFactory => Factories[CurrentSceneID];
 
-		public static Scene CurrentScene => Factories[CurrentSceneID].Scene;
 
-		public static SceneFactory CurrentFactory => Factories[CurrentSceneID];
+		int _barHeight = 64;
+		Color _barColor = Color.Black * 0.5f;
+		Color _textColor = Color.White;
 
-		public static void NextScene()
+		public SceneSwitcher(Layer layer) : base(layer)
+		{
+			
+		}
+
+
+		public override void Draw()
+		{
+			var canvasSize = GameMgr.WindowManager.CanvasSize;
+
+			GraphicsMgr.CurrentColor = _barColor;
+			RectangleShape.Draw(new Vector2(0, canvasSize.Y - _barHeight), canvasSize, false);
+
+			GraphicsMgr.CurrentColor = _textColor;
+			Text.CurrentFont = Resources.Fonts.Arial;
+			Text.HorAlign = TextAlign.Left;
+			Text.VerAlign = TextAlign.Top;
+			Text.Draw("fps: " + GameMgr.Fps, new Vector2(16, 16));
+
+			//Resources.Sprites.Default.Monofoxe.Draw(new Vector2(400, 300), Resources.Sprites.Default.Monofoxe.Origin);
+		}
+
+
+		public void NextScene()
 		{
 			Factories[CurrentSceneID].DestroyScene();
 
@@ -33,7 +63,7 @@ namespace Monofoxe.Playground.Interface
 		}
 
 
-		public static void PreviousScene()
+		public void PreviousScene()
 		{
 			Factories[CurrentSceneID].DestroyScene();
 
@@ -47,7 +77,7 @@ namespace Monofoxe.Playground.Interface
 		}
 
 
-		public static void RestartScene() =>
+		public void RestartScene() =>
 			Factories[CurrentSceneID].RestartScene();
 
 	}
