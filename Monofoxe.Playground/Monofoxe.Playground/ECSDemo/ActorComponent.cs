8916deb8dbd379319fe0916f9eeb969cf6c85f8a
gn Fur <21219159+gnFur@users.noreply.github.com>
2019-06-23 01:09:28 +0300

    Previous commit fix.

---
+++ Monofoxe.Playground/Monofoxe.Playground/ECSDemo/ActorComponent.cs
@@ -0,0 +1,26 @@
+﻿using Monofoxe.Engine.ECS;
+using Monofoxe.Engine.Drawing;
+
+namespace Monofoxe.Playground.ECSDemo
+{
+	/// <summary>
+	/// Actor component. Actors can move in some direction.
+	/// NOTE: PositionComponent is required.
+	/// </summary>
+	public class ActorComponent : Component
+	{
+		public bool Move = false;
+
+		public float Direction = 0;
+		public float Speed = 120;
+
+		public Sprite Sprite;
+		
+		public ActorComponent(Sprite sprite)
+		{
+			Visible = true; // Components are not visible by default.
+			
+			Sprite = sprite;
+		}
+	}
+}
---
+++ Monofoxe.Playground/Monofoxe.Playground/ECSDemo/PositionComponent.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using Monofoxe.Engine.ECS;
+
+namespace Monofoxe.Playground.ECSDemo
+{
+	/// <summary>
+	/// Basic position component. 
+	/// </summary>
+	public class PositionComponent : Component
+	{
+
+		/// <summary>
+		/// Entity position on the scene.
+		/// </summary>
+		public Vector2 Position;
+		
+		/// <summary>
+		/// Starting entity position on the scene.
+		/// </summary>
+		public Vector2 StartingPosition;
+
+		public PositionComponent(Vector2 position)
+		{
+			Position = position;
+			StartingPosition = position;
+		}
+	}
+}
