5791244c66b531655a2c6158407c8bd03e8336a8
gn.fur <patych7@gmail.com>
2019-06-23 01:09:28 +0300

    Previous commit fix.

---
+++ Monofoxe.Playground/Monofoxe.Playground/ECSDemo/ActorSystem.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Monofoxe.Engine.ECS;
+using Monofoxe.Engine.Utils;
+using Microsoft.Xna.Framework;
+using Monofoxe.Engine.Drawing;
+
+namespace Monofoxe.Playground.ECSDemo
+{
+	public class ActorSystem : BaseSystem
+	{
+		public override Type ComponentType => typeof(ActorComponent);
+
+		public override int Priority => 1;
+
+		public override void Create(Component component)
+		{
+
+		}
+
+
+		public override void Update(List<Component> components)
+		{
+			// During update system picks up all the components on the scene at once.
+			foreach(ActorComponent actor in components)
+			{
+				if (actor.Move)
+				{
+					// Retrieving the position component from entity.
+					var position = actor.Owner.GetComponent<PositionComponent>();
+
+					position.Position += TimeKeeper.GlobalTime(actor.Speed) * GameMath.DirectionToVector2(actor.Direction);
+				}
+			}
+		}
+
+		public override void Draw(Component component)
+		{
+			var actor = (ActorComponent)component;
+			var position = actor.Owner.GetComponent<PositionComponent>();
+		
+			GraphicsMgr.CurrentColor = Color.White;
+
+			actor.Sprite.Draw(position.Position, actor.Sprite.Origin);
+		}
+
+	}
+}
---
+++ Monofoxe.Playground/Monofoxe.Playground/ECSDemo/BotComponent.cs
@@ -0,0 +1,18 @@
+﻿using Monofoxe.Engine.ECS;
+using Monofoxe.Engine.Drawing;
+
+namespace Monofoxe.Playground.ECSDemo
+{
+	/// <summary>
+	/// Basic position component. 
+	/// </summary>
+	public class BotComponent : Component
+	{
+		public float TurningSpeed = 60;
+
+		public BotComponent()
+		{
+			
+		}
+	}
+}
