0e7efcdff8199513000864a63504a9f57c4b21a0
gn Fur <21219159+gnFur@users.noreply.github.com>
2019-06-24 01:24:40 +0300

    Simplified easings and added utilities demo.

---
+++ Monofoxe.Playground/Monofoxe.Playground/UtilsDemo/UtilsDemo.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Monofoxe.Engine.ECS;
+using Monofoxe.Engine.Drawing;
+using Monofoxe.Engine.Utils;
+using Monofoxe.Engine;
+using Monofoxe.Engine.SceneSystem;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Resources.Sprites;
+using System.Text.RegularExpressions;
+using Monofoxe.Engine.Cameras;
+
+namespace Monofoxe.Playground.UtilsDemo
+{
+	public enum TestStates
+	{
+		Green,
+		Blue,
+		Red,
+	}
+
+	// Note that not all the utilities and their functions are shown here.
+
+	public class UtilsDemo : Entity
+	{
+		
+		Color _mainColor = Color.White;
+		Color _secondaryColor = Color.Violet;
+
+		double _animationSpeed = 0.25;
+		
+		Animation _fireAnimation;
+		
+		TimeKeeper _slowTimeKeeper;
+
+		AutoAlarm _autoAlarm;
+
+		Alarm _slowAlarm;
+
+		double _alarmPeriod = 1;
+
+		bool _autoAlarmSwitch = false;
+		bool _slowAlarmSwitch = false;
+		bool _counterSwitch = false;
+
+		double _counter = 0;
+
+		Camera _camera;
+
+		StateMachine<TestStates> _stateMachine;
+		Color _color;
+		bool _isRectangle = false;
+		bool _isOutline = false;
+
+		RandomExt _random;
+
+		public UtilsDemo(Layer layer) : base(layer)
+		{
+
+			// Animation.
+
+			// Animation is more sophisticated way of implementing animations.
+			// It can be used for anything from sprite animation to UI element position.
+			_fireAnimation = new Animation();
+			_fireAnimation.Speed = 1;
+
+			// You can set an easing to make animation non-linear.
+			// If it's not set, animation will be linear.
+			_fireAnimation.Easing = Easing.EaseInCirc;
+
+			// You can trigger a method at the end of an animation.
+			_fireAnimation.AnimationEndEvent = FireAnimationEnd;
+			_fireAnimation.Start(true);
+			// Animation.
+
+			// Alarms.
+			_slowTimeKeeper = new TimeKeeper();
+			// Slowing down time for this time keeper.
+			_slowTimeKeeper.TimeMultiplier = 0.5f;
+
+			_autoAlarm = new AutoAlarm(_alarmPeriod);
+
+			_slowAlarm = new Alarm();
+			// This alarm will now use custom TimeKeeper, which is 2 times 
+			// slower than global TimeKeeper. 
+			_slowAlarm.TimeKeeper = _slowTimeKeeper;
+			_slowAlarm.Set(_alarmPeriod);
+			_slowAlarm.TriggerAction = AlarmTrigger;
+			// Alarms.
+
+			// Camera.
+			_camera = new Camera(400, 600);
+			_camera.PortPosition = new Vector2(400, 0);
+			_camera.BackgroundColor = Color.Black;
+			_camera.PostprocessorEffects.Add(Resources.Effects.Grayscale);
+			_camera.PostprocessingMode = PostprocessingMode.Camera;
+			// Camera.
+
+			_random = new RandomExt();
+
+			// State machine.
+			// State machines are very useful for animation and complex logic.
+			_stateMachine = new StateMachine<TestStates>(TestStates.Green, this);
+			
+			// Filling up the state machine with events.
+			_stateMachine.AddState(TestStates.Green, Green, GreenEnter, GreenExit);
+			_stateMachine.AddState(TestStates.Blue, Blue);
+			_stateMachine.AddState(TestStates.Red, Red);
+			// State machine.
+		}
+
+		/// <summary>
+		/// Triggerrs when animation loops.
+		/// </summary>
+		public void FireAnimationEnd(Animation animation)
+		{
+			// Inverting animation direction and setting appropriate easing.
+			_fireAnimation.Invert = !_fireAnimation.Invert;
+			if (_fireAnimation.Invert)
+			{
+				_fireAnimation.Easing = Easing.EaseOutCirc;
+			}
+			else
+			{
+				_fireAnimation.Easing = Easing.EaseInCirc;
+			}
+
+		}
+
+		/// <summary>
+		/// Triggers when alarm runs out.
+		/// </summary>
+		public void AlarmTrigger(Alarm alarm)
+		{
+			// This is actually a bad practice. 
+			// Alarm doesn't take into account leftover time
+			// and will reset counter to 0. This will introduce errors,
+			// which will add up over time. If you need to set Alarm 
+			// right after it triggers, use AutoAlarm.
+			//
+			// This demo shows comparison of Alarm and AutoAlarm.
+			// You will notice that circles start blinking out of sync
+			// over time.
+			alarm.Set(_alarmPeriod); 
+
+			_slowAlarmSwitch = !_slowAlarmSwitch;
+		}
+
+
+		#region State machine.
+
+		public void GreenEnter(StateMachine<TestStates> stateMachine, Entity caller)
+		{
+			// Here caller isn't really used, but it will be necessary if you're using ECS.
+			_isRectangle = !_isRectangle;
+		}
+		public void Green(StateMachine<TestStates> stateMachine, Entity caller)
+		{
+			_color = Color.Green;
+			if (_random.NextDouble() > 0.9)
+			{
+				stateMachine.ChangeState(TestStates.Blue);
+				return;
+			}
+			if (_random.NextDouble() > 0.8)
+			{
+				stateMachine.ChangeState(TestStates.Red);
+				return;
+			}
+		}
+		public void GreenExit(StateMachine<TestStates> stateMachine, Entity caller)
+		{
+			_isOutline = !_isOutline;
+		}
+
+		public void Blue(StateMachine<TestStates> stateMachine, Entity caller)
+		{
+			_color = Color.Blue;
+			if (_random.NextDouble() > 0.9)
+			{
+				stateMachine.ChangeState(TestStates.Red);
+				return;
+			}
+		}
+
+		public void Red(StateMachine<TestStates> stateMachine, Entity caller)
+		{
+			_color = Color.Red;
+			if (_random.NextDouble() > 0.97)
+			{
+				stateMachine.ChangeState(TestStates.Green);
+				return;
+			}
+		}
+
+		#endregion State machine.
+
+
+		public override void Update()
+		{
+			// It needs to be updated automatically.
+			_fireAnimation.Update();
+
+			_slowAlarm.Update();
+
+			// You also can not use TriggerAction and check if the alarm was triggered this way.
+			if (_autoAlarm.Update())
+			{
+				_autoAlarmSwitch = !_autoAlarmSwitch;
+			}
+
+
+			// You can count time by hand. Not that you should, it just
+			// shows how time keepers are used internally.
+			// It is usually used for different tasks, which alarms can't really handle.
+			_counter += _slowTimeKeeper.Time();
+			if (_counter > _alarmPeriod)
+			{
+				// Subtracting instead of zeroing will correct for time errors.
+				_counter -= _alarmPeriod;
+				_counterSwitch = !_counterSwitch;
+			}
+
+			_stateMachine.Update();
+
+		}
+
+
+		public override void Draw()
+		{
+			var startingPosition = new Vector2(64, 64);
+			var position = startingPosition;
+			var spacing = 100;
+
+			GraphicsMgr.CurrentColor = Color.White;
+			
+			Default.Fire.Draw(_fireAnimation.Progress, position, Vector2.Zero);
+			
+			position += Vector2.UnitX * spacing;
+			CircleShape.Draw(position, 8, _autoAlarmSwitch);
+
+			position += Vector2.UnitX * 32;
+			CircleShape.Draw(position, 8, _slowAlarmSwitch);
+
+			position += Vector2.UnitX * 32;
+			CircleShape.Draw(position, 8, _counterSwitch);
+
+			position += Vector2.UnitX * 32;
+			GraphicsMgr.CurrentColor = _color;
+			if (_isRectangle)
+			{
+				RectangleShape.DrawBySize(position, Vector2.One * 16, _isOutline);
+			}
+			else
+			{
+				CircleShape.Draw(position, 8, _isOutline);
+			}
+
+
+
+		}
+
+		public override void Destroy()
+		{
+			_camera.Destroy();
+		}
+
+
+	}
+}
--- Monofoxe/Monofoxe.Engine/Utils/Easing.cs
+++ Monofoxe/Monofoxe.Engine/Utils/Easing.cs
@@ -15,43 +15,15 @@ namespace Monofoxe.Engine.Utils
 	public class Easing
 	{
 		/// <summary>
-		/// Determines, which easing should be used.
-		/// </summary>
-		public EasingTypes Type
-		{
-			get => _type;
-			set
-			{
-				if (_funcs.TryGetValue(value, out Func<double, double> func))
-				{
-					_type = value;
-					Func = func;
-				}
-				else
-				{
-					_type = EasingTypes.None;
-					Func = null;
-				}
-			}
-		}
-		private EasingTypes _type;
-
-		/// <summary>
 		/// Current easing function.
 		/// </summary>
-		public Func<double, double> Func {get; private set;}
+		public readonly Func<double, double> Func;
 		
-		public Easing(EasingTypes type)
-		{
-			Type = type;
-		}
-
 		/// <summary>
-		/// Sets custon easing function.
+		/// This constructor is used to create easings. You can create your own!
 		/// </summary>
-		public void SetCustomEasing(Func<double, double> func)
+		public Easing(Func<double, double> func)
 		{
-			_type = EasingTypes.Custom;
 			Func = func;
 		}
 
@@ -67,100 +39,102 @@ namespace Monofoxe.Engine.Utils
 		/// </summary>
 		public double GetEasing(double t) => Func(t);
 
+		#region Default easings.
+
+		public static readonly Easing EaseInSine = new Easing(EaseInSineFunc);
+		public static readonly Easing EaseOutSine = new Easing(EaseOutSineFunc);
+		public static readonly Easing EaseInOutSine = new Easing(EaseInOutSineFunc);
+		public static readonly Easing EaseInQuad = new Easing(EaseInQuadFunc);
+		public static readonly Easing EaseOutQuad = new Easing(EaseOutQuadFunc);
+		public static readonly Easing EaseInOutQuad = new Easing(EaseInOutQuadFunc);
+		public static readonly Easing EaseInCubic = new Easing(EaseInCubicFunc);
+		public static readonly Easing EaseOutCubic = new Easing(EaseOutCubicFunc);
+		public static readonly Easing EaseInOutCubic = new Easing(EaseInOutCubicFunc);
+		public static readonly Easing EaseInQuart = new Easing(EaseInQuartFunc);
+		public static readonly Easing EaseOutQuart = new Easing(EaseOutQuartFunc);
+		public static readonly Easing EaseInOutQuart = new Easing(EaseInOutQuartFunc);
+		public static readonly Easing EaseInQuint = new Easing(EaseInQuintFunc);
+		public static readonly Easing EaseOutQuint = new Easing(EaseOutQuintFunc);
+		public static readonly Easing EaseInOutQuint = new Easing(EaseInOutQuintFunc);
+		public static readonly Easing EaseInExpo = new Easing(EaseInExpoFunc);
+		public static readonly Easing EaseOutExpo = new Easing(EaseOutExpoFunc);
+		public static readonly Easing EaseInOutExpo = new Easing(EaseInOutExpoFunc);
+		public static readonly Easing EaseInCirc = new Easing(EaseInCircFunc);
+		public static readonly Easing EaseOutCirc = new Easing(EaseOutCircFunc);
+		public static readonly Easing EaseInOutCirc = new Easing(EaseInOutCircFunc);
+		public static readonly Easing EaseInBack = new Easing(EaseInBackFunc);
+		public static readonly Easing EaseOutBack = new Easing(EaseOutBackFunc);
+		public static readonly Easing EaseInOutBack = new Easing(EaseInOutBackFunc);
+		public static readonly Easing EaseInElastic = new Easing(EaseInElasticFunc);
+		public static readonly Easing EaseOutElastic = new Easing(EaseOutElasticFunc);
+		public static readonly Easing EaseInOutElastic = new Easing(EaseInOutElasticFunc);
+		public static readonly Easing EaseInBounce = new Easing(EaseInBounceFunc);
+		public static readonly Easing EaseOutBounce = new Easing(EaseOutBounceFunc);
+		public static readonly Easing EaseInOutBounce = new Easing(EaseInOutBounceFunc);
+
+		#endregion Default easings.
+
+
 		#region Easing functions.
 
-		static Dictionary<EasingTypes, Func<double, double>> _funcs = new Dictionary<EasingTypes, Func<double, double>>
-		{
-			{EasingTypes.EaseInSine, EaseInSine},
-			{EasingTypes.EaseOutSine, EaseOutSine},
-			{EasingTypes.EaseInOutSine, EaseInOutSine},
-			{EasingTypes.EaseInQuad, EaseInQuad},
-			{EasingTypes.EaseOutQuad, EaseOutQuad},
-			{EasingTypes.EaseInOutQuad, EaseInOutQuad},
-			{EasingTypes.EaseInCubic, EaseInCubic},
-			{EasingTypes.EaseOutCubic, EaseOutCubic},
-			{EasingTypes.EaseInOutCubic, EaseInOutCubic},
-			{EasingTypes.EaseInQuart, EaseInQuart},
-			{EasingTypes.EaseOutQuart, EaseOutQuart},
-			{EasingTypes.EaseInOutQuart, EaseInOutQuart},
-			{EasingTypes.EaseInQuint, EaseInQuint},
-			{EasingTypes.EaseOutQuint, EaseOutQuint},
-			{EasingTypes.EaseInOutQuint, EaseInOutQuint},
-			{EasingTypes.EaseInExpo, EaseInExpo},
-			{EasingTypes.EaseOutExpo, EaseOutExpo},
-			{EasingTypes.EaseInOutExpo, EaseInOutExpo},
-			{EasingTypes.EaseInCirc, EaseInCirc},
-			{EasingTypes.EaseOutCirc, EaseOutCirc},
-			{EasingTypes.EaseInOutCirc, EaseInOutCirc},
-			{EasingTypes.EaseInBack, EaseInBack},
-			{EasingTypes.EaseOutBack, EaseOutBack},
-			{EasingTypes.EaseInOutBack, EaseInOutBack},
-			{EasingTypes.EaseInElastic, EaseInElastic},
-			{EasingTypes.EaseOutElastic, EaseOutElastic},
-			{EasingTypes.EaseInOutElastic, EaseInOutElastic},
-			{EasingTypes.EaseInBounce, EaseInBounce},
-			{EasingTypes.EaseOutBounce, EaseOutBounce},
-			{EasingTypes.EaseInOutBounce, EaseInOutBounce},
-		};
-
-		public static double EaseInSine(double t)
+		public static double EaseInSineFunc(double t)
 		{
 			return Math.Sin(1.5707963 * t);
 		}
 
-		public static double EaseOutSine(double t)
+		public static double EaseOutSineFunc(double t)
 		{
 			return 1 + Math.Sin(1.5707963 * (--t));
 		}
 
-		public static double EaseInOutSine(double t)
+		public static double EaseInOutSineFunc(double t)
 		{
 			return 0.5 * (1 + Math.Sin(3.1415926 * (t - 0.5)));
 		}
 
-		public static double EaseInQuad(double t)
+		public static double EaseInQuadFunc(double t)
 		{
 			return t * t;
 		}
 
-		public static double EaseOutQuad(double t)
+		public static double EaseOutQuadFunc(double t)
 		{
 			return t * (2 - t);
 		}
 
-		public static double EaseInOutQuad(double t)
+		public static double EaseInOutQuadFunc(double t)
 		{
 			return t < 0.5 ? 2 * t * t : t * (4 - 2 * t) - 1;
 		}
 
-		public static double EaseInCubic(double t)
+		public static double EaseInCubicFunc(double t)
 		{
 			return t * t * t;
 		}
 
-		public static double EaseOutCubic(double t)
+		public static double EaseOutCubicFunc(double t)
 		{
 			return 1 + (--t) * t * t;
 		}
 
-		public static double EaseInOutCubic(double t)
+		public static double EaseInOutCubicFunc(double t)
 		{
 			return t < 0.5 ? 4 * t * t * t : 1 + (--t) * (2 * (--t)) * (2 * t);
 		}
 
-		public static double EaseInQuart(double t)
+		public static double EaseInQuartFunc(double t)
 		{
 			t *= t;
 			return t * t;
 		}
 
-		public static double EaseOutQuart(double t)
+		public static double EaseOutQuartFunc(double t)
 		{
 			t = (--t) * t;
 			return 1 - t * t;
 		}
 
-		public static double EaseInOutQuart(double t)
+		public static double EaseInOutQuartFunc(double t)
 		{
 			if (t < 0.5)
 			{
@@ -174,19 +148,19 @@ namespace Monofoxe.Engine.Utils
 			}
 		}
 
-		public static double EaseInQuint(double t)
+		public static double EaseInQuintFunc(double t)
 		{
 			double t2 = t * t;
 			return t * t2 * t2;
 		}
 
-		public static double EaseOutQuint(double t)
+		public static double EaseOutQuintFunc(double t)
 		{
 			double t2 = (--t) * t;
 			return 1 + t * t2 * t2;
 		}
 
-		public static double EaseInOutQuint(double t)
+		public static double EaseInOutQuintFunc(double t)
 		{
 			double t2;
 			if (t < 0.5)
@@ -201,17 +175,17 @@ namespace Monofoxe.Engine.Utils
 			}
 		}
 
-		public static double EaseInExpo(double t)
+		public static double EaseInExpoFunc(double t)
 		{
 			return (Math.Pow(2, 8 * t) - 1) / 255;
 		}
 
-		public static double EaseOutExpo(double t)
+		public static double EaseOutExpoFunc(double t)
 		{
 			return 1 - Math.Pow(2, -8 * t);
 		}
 
-		public static double EaseInOutExpo(double t)
+		public static double EaseInOutExpoFunc(double t)
 		{
 			if (t < 0.5)
 			{
@@ -223,17 +197,17 @@ namespace Monofoxe.Engine.Utils
 			}
 		}
 
-		public static double EaseInCirc(double t)
+		public static double EaseInCircFunc(double t)
 		{
 			return 1 - Math.Sqrt(1 - t);
 		}
 
-		public static double EaseOutCirc(double t)
+		public static double EaseOutCircFunc(double t)
 		{
 			return Math.Sqrt(t);
 		}
 
-		public static double EaseInOutCirc(double t)
+		public static double EaseInOutCircFunc(double t)
 		{
 			if (t < 0.5)
 			{
@@ -245,17 +219,17 @@ namespace Monofoxe.Engine.Utils
 			}
 		}
 
-		public static double EaseInBack(double t)
+		public static double EaseInBackFunc(double t)
 		{
 			return t * t * (2.70158 * t - 1.70158);
 		}
 
-		public static double EaseOutBack(double t)
+		public static double EaseOutBackFunc(double t)
 		{
 			return 1 + (--t) * t * (2.70158 * t + 1.70158);
 		}
 
-		public static double EaseInOutBack(double t)
+		public static double EaseInOutBackFunc(double t)
 		{
 			if (t < 0.5)
 			{
@@ -267,19 +241,19 @@ namespace Monofoxe.Engine.Utils
 			}
 		}
 
-		public static double EaseInElastic(double t)
+		public static double EaseInElasticFunc(double t)
 		{
 			double t2 = t * t;
 			return t2 * t2 * Math.Sin(t * Math.PI * 4.5);
 		}
 
-		public static double EaseOutElastic(double t)
+		public static double EaseOutElasticFunc(double t)
 		{
 			double t2 = (t - 1) * (t - 1);
 			return 1 - t2 * t2 * Math.Cos(t * Math.PI * 4.5);
 		}
 
-		public static double EaseInOutElastic(double t)
+		public static double EaseInOutElasticFunc(double t)
 		{
 			double t2;
 			if (t < 0.45)
@@ -298,17 +272,17 @@ namespace Monofoxe.Engine.Utils
 			}
 		}
 
-		public static double EaseInBounce(double t)
+		public static double EaseInBounceFunc(double t)
 		{
 			return Math.Pow(2, 6 * (t - 1)) * Math.Abs(Math.Sin(t * Math.PI * 3.5));
 		}
 
-		public static double EaseOutBounce(double t)
+		public static double EaseOutBounceFunc(double t)
 		{
 			return 1 - Math.Pow(2, -6 * t) * Math.Abs(Math.Cos(t * Math.PI * 3.5));
 		}
 
-		public static double EaseInOutBounce(double t)
+		public static double EaseInOutBounceFunc(double t)
 		{
 			if (t < 0.5)
 			{
