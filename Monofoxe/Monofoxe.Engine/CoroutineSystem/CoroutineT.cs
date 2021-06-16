1fbebf4cf18d96077576ea94d75df1cc22f72497
Félix Dion-Robidoux (@fdrobidoux) <felix@fdrobidoux.com>
2021-06-15 21:38:27 -0400

    Implemented Coroutine system
    Very basic. Still needs more tests

---
+++ Monofoxe/Monofoxe.Engine/CoroutineSystem/CoroutineT.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Monofoxe.Engine.CoroutineSystem
+{
+	public class Coroutine<T> : Coroutine
+	{
+		internal override void Reset(IEnumerator routine)
+		{
+			base.Reset(routine);
+			OnYieldValue = null;
+		}
+
+		internal override bool Update()
+		{
+			if (Paused) return true;
+
+			if (RoutinesStack.Peek().MoveNext())
+			{
+				if (RoutinesStack.Peek().Current is IEnumerator<T> enumerator)
+				{
+					RoutinesStack.Push(enumerator);
+				}
+				else if (RoutinesStack.Peek().Current is T valueT)
+				{
+					OnYieldValue?.Invoke(this, valueT);
+				}
+			}
+			else
+			{
+				RoutinesStack.Pop();
+				if (RoutinesStack.Count == 0)
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
+
+		public event EventHandler<T> OnYieldValue;
+
+		public override void Dispose()
+		{
+			base.Dispose();
+			OnYieldValue = null;
+		}
+	}
+}
---
+++ Monofoxe/Monofoxe.Tests/CoroutineTests.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.Xna.Framework;
+using Monofoxe.Engine.Utils;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using Monofoxe.Engine.CoroutineSystem;
+
+namespace Tests
+{
+	[ExcludeFromCodeCoverage]
+	public class CoroutineTests
+	{
+		[SetUp]
+		public void Setup()
+		{
+			CoroutineMgr.activeCoroutines = new List<Coroutine>();
+			CoroutineMgr.incomingCoroutines = new List<Coroutine>();
+		}
+
+		private static void DoFullUpdate()
+		{
+			CoroutineMgr.PreUpdateRoutine();
+			CoroutineMgr.UpdateCoroutines();
+			CoroutineMgr.PostUpdateRoutine();
+		}
+
+		[Test]
+		public void StartsUp_CanMoveNext()
+		{
+			var coroutine = CoroutineMgr.StartCoroutine(TestEnumerator());
+
+			Assert.True(coroutine.RoutinesStack.Peek().MoveNext());
+		}
+		
+		[Test]
+		public void AfterFullUpdate_CannotMoreFurther()
+		{
+			var coroutine = CoroutineMgr.StartCoroutine(TestEnumerator());
+			
+			DoFullUpdate();
+			
+			var enumerator = (coroutine.RoutinesStack.Peek() as IEnumerator);
+			Assert.NotNull(enumerator);
+			Assert.False(enumerator.MoveNext());
+		}
+
+		[Test]
+		public void StillInStack_IfPaused()
+		{
+			var coroutine = CoroutineMgr.StartCoroutine(TestEnumerator());
+			coroutine.Paused = true;
+			
+			DoFullUpdate();
+			
+			Assert.True(CoroutineMgr.activeCoroutines.Contains(coroutine));
+		}
+
+		[Test]
+		public void GetsRemovedFromStack_WhenItsOver()
+		{
+			var coroutine = CoroutineMgr.StartCoroutine(TestEnumerator());
+			
+			DoFullUpdate();
+			DoFullUpdate();
+			
+			Assert.True(CoroutineMgr.WasRemoved(coroutine));
+		}
+
+		[Test]
+		public void BreaksEarly_WhenSpecified()
+		{
+			var coroutine = CoroutineMgr.StartCoroutine(TestEnumeratorBreaksOnNegatives(new []{ 1, 2, 3, 4, -20, 1 }));
+
+			DoFullUpdate();
+			DoFullUpdate();
+			DoFullUpdate();
+			DoFullUpdate();
+			
+			Assert.AreEqual(1, coroutine.RoutinesStack.Count);
+			
+			DoFullUpdate();
+			
+			Assert.AreEqual(0, coroutine.RoutinesStack.Count);
+		}
+
+		public IEnumerator TestEnumerator()
+		{
+			var ok = false;
+			
+			while (!ok)
+			{
+				yield return null;
+				ok = true;
+			}
+		}
+		
+		public IEnumerator<int> TestEnumeratorBreaksOnNegatives(int[] sequence)
+		{
+			foreach (int i in sequence)
+			{
+				if (i < 0) yield break;
+				yield return i;
+			}
+		}
+	}
+}
