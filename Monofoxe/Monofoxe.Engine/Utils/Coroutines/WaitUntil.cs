3a06935e31503b236ff3e7b7978b7b87671105c9
Martenfur <patych7@gmail.com>
2021-08-07 13:06:34 +0300

    Removed WaitForButton and added WaitWhile and WaitUntil.

---
+++ Monofoxe/Monofoxe.Engine/Utils/Coroutines/WaitUntil.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+namespace Monofoxe.Engine.Utils.Coroutines
+{
+	/// <summary>
+	/// Suspends the coroutine execution until the supplied delegate evaluates to false.
+	/// </summary>
+	public class WaitWhile : YieldInstruction
+	{
+		private Func<bool> _action;
+
+		public WaitWhile(Func<bool> action)
+		{
+			_action = action;
+		}
+
+
+		public override IEnumerator Run()
+		{
+			while (_action())
+			{
+				yield return null;
+			}
+		}
+	}
+}
