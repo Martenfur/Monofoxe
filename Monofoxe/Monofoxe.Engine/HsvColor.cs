a726ad9c2189e187c3aefda0c3ae097599021d8b
Martenfur <patych7@gmail.com>
2022-01-21 00:45:00 +0100

    Expanded HvsColor features.

--- Monofoxe/Monofoxe.Engine/HsvColor.cs
+++ Monofoxe/Monofoxe.Engine/HsvColor.cs
@@ -1,40 +1,41 @@
 ﻿using Microsoft.Xna.Framework;
+using System;
 
 namespace Monofoxe.Engine
 {
-  public struct HsvColor
+	public struct HsvColor : IEquatable<HsvColor>, IComparable<HsvColor>
 	{
-		public float H 
-		{ 
-			get => _h; 
-			set => _h = MathHelper.Clamp(value, 0, 360); 
+		public float H
+		{
+			get => _h;
+			set => _h = MathHelper.Clamp(value, 0, 360);
 		}
 		public float _h;
 
-		public float S 
-		{ 
-			get => _s; 
-			set => _s = MathHelper.Clamp(value, 0, 1); 
+		public float S
+		{
+			get => _s;
+			set => _s = MathHelper.Clamp(value, 0, 1);
 		}
 		public float _s;
 
-		public float V 
-		{ 
-			get => _v; 
-			set => _v = MathHelper.Clamp(value, 0, 1); 
+		public float V
+		{
+			get => _v;
+			set => _v = MathHelper.Clamp(value, 0, 1);
 		}
 		public float _v;
 
-		public float A 
-		{ 
-			get => _a; 
-			set => _a = MathHelper.Clamp(value, 0, 1); 
+		public float A
+		{
+			get => _a;
+			set => _a = MathHelper.Clamp(value, 0, 1);
 		}
 		public float _a;
 
 
 		public HsvColor(Color color)
-		{ 
+		{
 			var hsv = color.ToHsvColor();
 			_h = hsv.H;
 			_s = hsv.S;
@@ -89,6 +90,61 @@ namespace Monofoxe.Engine
 					return new Color(V, p, q, A);
 			}
 			return Color.Black; // Keeps the compiler happy.
+		}
+
+
+		public override bool Equals(object obj)
+		{
+			if (obj is HsvColor)
+			{
+				return Equals((HsvColor)obj);
+			}
+
+			return base.Equals(obj);
+		}
+
+
+		public bool Equals(HsvColor value) =>
+			H.Equals(value.H) && S.Equals(value.S) && V.Equals(value.V);
+
+
+		public override int GetHashCode() =>
+			H.GetHashCode() ^ S.GetHashCode() ^ V.GetHashCode();
+
+
+		public int CompareTo(HsvColor other) =>
+			H.CompareTo(other.H) * 100 + S.CompareTo(other.S) * 10 + V.CompareTo(V);
+
+
+		public void CopyTo(out HsvColor destination) =>
+			destination = new HsvColor(H, S, V);
+
+
+		public static HsvColor operator +(HsvColor a, HsvColor b) =>
+			new HsvColor(a.H + b.H, a.S + b.S, a.V + b.V);
+
+		public static bool operator ==(HsvColor x, HsvColor y) =>
+			x.Equals(y);
+		
+
+
+		public static bool operator !=(HsvColor x, HsvColor y) =>
+			!x.Equals(y);
+
+
+		public static HsvColor operator -(HsvColor a, HsvColor b) =>
+			new HsvColor(a.H - b.H, a.S - b.S, a.V - b.V);
+
+
+		public static HsvColor Lerp(HsvColor c1, HsvColor c2, float t)
+		{
+			// loop around if c2.H < c1.H
+			var h2 = c2.H >= c1.H ? c2.H : c2.H + 360;
+			return new HsvColor(
+				c1.H + t * (h2 - c1.H),
+				c1.S + t * (c2.S - c1.S),
+				c1.V + t * (c2.V - c2.V)
+			);
 		}
 	}
 }
