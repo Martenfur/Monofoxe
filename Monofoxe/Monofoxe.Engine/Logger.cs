106251caff84924db621f280a98ebb7baad29201
Martenfur <patych7@gmail.com>
2022-09-16 22:25:50 +0200

    Added various utils.

---
+++ Monofoxe/Monofoxe.Engine/Logger.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Monofoxe.Engine
+{
+	public delegate void LoggerDelegate(string log, LogLevel level);
+
+	public static class Logger
+	{
+		public static LogLevel LogFilter = LogLevel.All;
+
+		public static event LoggerDelegate OnLog;
+
+		/// <summary>
+		/// If true, skips a newline at the end of next log.
+		/// </summary>
+		private static bool _sameLine = false;
+
+		public static void Log(object log, LogLevel level = LogLevel.Info)
+		{
+			if (OnLog == null || (level & LogFilter) == 0)
+			{
+				return;
+			}
+
+			if (_sameLine)
+			{
+				_sameLine = false;
+				OnLog.Invoke(log.ToString(), level);
+			}
+			else
+			{
+				OnLog.Invoke(log.ToString() + Environment.NewLine, level);
+			}
+		}
+
+		/// <summary>
+		/// Skips a newline at the end of next log.
+		/// </summary>
+		public static void SameLine() =>
+			_sameLine = true;
+	}
+}
---
+++ Monofoxe/Monofoxe.Engine/Utils/SlowRotator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Monofoxe.Engine.Utils
+{
+	/// <summary>
+	/// Gradually rotates the angle towards the target.
+	/// </summary>
+	public class SlowRotator
+	{
+		public TimeKeeper Time = TimeKeeper.Global;
+
+		public Angle Direction;
+
+		/// <summary>
+		/// The speed with which rotator will rotate. Measured in deg/sec.
+		/// </summary>
+		public double RotationSpeed;
+
+		public SlowRotator(double rotationSpeed)
+		{
+			RotationSpeed = rotationSpeed;
+		}
+
+		public void Update(Angle targetDirection)
+		{
+			var delta = Direction.Difference(targetDirection);
+
+			var frameTurnSpeed = Time.Time(RotationSpeed);
+
+			if (Math.Abs(delta) > frameTurnSpeed)
+			{
+				Direction -= frameTurnSpeed * Math.Sign(delta);
+			}
+			else
+			{
+				Direction = targetDirection;
+			}
+		}
+	}
+}
