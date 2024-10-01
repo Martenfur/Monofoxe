d58da22ef1befa71d1dea72077ed54b921726da8
minkberry <chaifoxes@gmail.com>
2024-10-01 21:35:31 +0200

    Added position component.

---
+++ Monofoxe/Monofoxe.Engine/Utils/PositionComponent.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using Monofoxe.Engine.EC;
+
+
+namespace Monofoxe.Engine.Utils
+{
+	public class PositionComponent : Component
+	{
+		[Inspectable]
+		public Vector2 Value;
+		[Inspectable]
+		public float Depth;
+		[Inspectable(false)]
+		public Vector2 OldValue;
+		[Inspectable(false)]
+		public float OldDepth;
+
+		public PositionComponent(Vector2 position, float depth = 0)
+		{
+			Value = position;
+			Depth = depth;
+		}
+
+		public override void Update()
+		{
+			base.Update();
+			OldValue = Value;
+			OldDepth = Depth;
+		}
+	}
+}
